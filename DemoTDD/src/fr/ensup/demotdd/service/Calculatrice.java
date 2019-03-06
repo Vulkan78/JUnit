88a6ce6be0b26b7e0fb5e2d6d365c92c7b907e5b
Vulkan78 <jimmy.rakotoson@gmail.com>
2019-03-06 17:14:04 +0100

    V0.2
    - Test scenarioAdditionEntiers rédigé
    - Ajout classe Calculatrice 
    - Ajout méthode AdditionEntiers dans calculatrice

---
+++ DemoTDD/src/fr/ensup/demotdd/service/Calculatrice.java
@@ -0,0 +1,12 @@
+package fr.ensup.demotdd.service;
+
+/**
+ * Created by jimmy on 06/03/2019.
+ */
+public class Calculatrice {
+
+
+    public static int ajouterEntiers(int i, int j) {
+        return i+j;
+    }
+}
--- DemoTDD/src/fr/ensup/demotdd/test/CalculatriceTest.java
+++ DemoTDD/src/fr/ensup/demotdd/test/CalculatriceTest.java
@@ -1,5 +1,7 @@
 package fr.ensup.demotdd.test;
 
+import fr.ensup.demotdd.service.Calculatrice;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
@@ -11,7 +13,10 @@ public class CalculatriceTest {
 
     @Test
     public void scenarioAdditionEntiers(){
-        fail ("echec");
+        //fail ("echec");
+        Calculatrice myCalc = new Calculatrice();
+        int res = Calculatrice.ajouterEntiers(3,3);
+        Assert.assertEquals(res, 5);
         }
     }
 
