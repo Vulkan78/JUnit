fb1d9c51a73700a59fcae995b45ed703cccd0971
Vulkan78 <jimmy.rakotoson@gmail.com>
2019-03-06 16:59:31 +0100

    V0.1
    Classe test créé (fail)

---
+++ DemoTDD/src/fr/ensup/demotdd/test/CalculatriceTest.java
@@ -0,0 +1,17 @@
+package fr.ensup.demotdd.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by jimmy on 06/03/2019.
+ */
+public class CalculatriceTest {
+
+    @Test
+    public void scenarioAdditionEntiers(){
+        fail ("echec");
+        }
+    }
+
